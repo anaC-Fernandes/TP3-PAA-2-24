ce9e973a499f5ca5c95ac20ce701369c6628e433
anaC-Fernandes <ana.clementino@ufv.br>
2025-01-25 15:53:11 -0300

    Versão inicial do BMH

---
+++ src/BMH.c
@@ -0,0 +1,55 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+
+void bmh(char texto[], long tamTexto, char padrao[], long tamPadrao) { 
+    
+    if (tamPadrao > tamTexto) {
+        printf("O padrão é maior que o texto. Não há casamento possível.\n");
+        return;
+    }
+
+
+    //Tabela de deslocamento
+    int tabelaDeslocamento[256]; 
+    for (int j = 0; j < 256; j++) { 
+        //Inicializa todas as posicoes como tamanho do padrao
+        tabelaDeslocamento[j] = tamPadrao; 
+    } 
+    for (int j = 0; j < tamPadrao - 1; j++) { 
+        //Para os primeiro tamPadrao - 1 sao definidos os valores da tabela
+        //A tabela de deslocamento salva a última ocorrência da letra
+        //Da direita para a esquerda
+        tabelaDeslocamento[(int)padrao[j]] = tamPadrao - j - 1; 
+    } 
+
+    //Comparacoes
+    int i = tamPadrao - 1; 
+    while (i < tamTexto) { 
+        int k = i; 
+        int j = tamPadrao - 1; 
+        while ((j >= 0) && (texto[k] == padrao[j])) { 
+            //Diminui j e k enquanto forem casando
+            j--; 
+            k--; 
+        } 
+        if (j < 0) { 
+            //Caso tenha percorrido todo o padrao e tenha casado todas
+            //Entao j sera menor que 0
+            printf("Casamento na posicao :  %d\n", k + 1);
+        }
+        //Caso tenha encontrado uma colisao
+        //Procura pela tabela o deslocamento da letra que causou a colisao
+        //Soma com a posicao atual e efetua o deslocamento
+        //Reiniciando as comparacoes 
+        i = i + tabelaDeslocamento[(int)texto[i]]; 
+    }
+}
+
+void converteMaiuscula(char* texto){
+    for(int i = 0; texto[i] != '\0'; i++){
+        if((int)texto[i] >= 65 && (int)texto[i] <= 90){
+            texto[i] = (char)((int)texto[i] + 32);
+        }
+    }
+}
--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "Leitura.c"
+#include "BMH.c"
 
 int main() {
     char nomeArquivo[256];
@@ -11,4 +13,37 @@ int main() {
     strcpy(nomeArquivo, "../lib/texto.txt");
     char *texto = LeituraTexto(nomeArquivo);
     printf("%s", texto);
+
+    //Para calcular o tempo de execucao usamos o clock
+    clock_t inicio, final;
+    double tempo_gasto;
+
+
+    //char texto[] = "AABCACCACBAC"; 
+    char* padrao = (char*)malloc(250 * sizeof(char)); 
+
+    long tamTexto = strlen(texto);
+    converteMaiuscula(texto);
+
+    long tamPadrao; 
+
+    printf("\nO que deseja procurar?\n");
+    scanf("%s", padrao);
+    tamPadrao = strlen(padrao);
+    converteMaiuscula(padrao);
+
+    
+
+    printf("\nCom Boyer-Moore:\n");
+    //Inicio da contagem
+    inicio = clock();
+
+    bmh(texto, tamTexto, padrao, tamPadrao);
+    //Final da contagem
+    final = clock();
+
+    //Para achar o tempo gasto em segundos, dividimos o intervalor por clocks por segundos
+    tempo_gasto = ((double) (final - inicio)) / CLOCKS_PER_SEC;
+    printf("Tempo gasto: %f segundos\n", tempo_gasto);
+    
 }
