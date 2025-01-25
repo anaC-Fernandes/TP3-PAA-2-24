62cd542a9b7d029c702f53f5678c67cda164c31e
anaC-Fernandes <ana.clementino@ufv.br>
2025-01-25 15:41:31 -0300

    Altera nome do arquivo

--- src/main.c
+++ src/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "Leitura.h"
+#include "Leitura.c"
 
 int main() {
     char nomeArquivo[256];
     //printf("Digite o nome do arquivo a ser lido: ");
     //scanf("%s", fileName);
 
-    strcpy(nomeArquivo, "C:\\Users\\gabri\\OneDrive\\Documentos\\GitHub\\TP3-PAA-2-24\\lib\\texto.txt");
+    strcpy(nomeArquivo, "../lib/texto.txt");
     char *texto = LeituraTexto(nomeArquivo);
     printf("%s", texto);
 }
